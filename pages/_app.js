46e9cf637c11a701f7bc8299d79f0bd2b2b21c76
JoakBouy <B.joak@alustudent.com>
2023-06-11 02:05:27 +0300

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 import '../styles/globals.css'
 
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => page);
+  return (
+  <UserProvider>
+    {getLayout(<Component {...pageProps} />, pageProps)}
+  </UserProvider>
+  );
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,31 @@
+import Link from 'next/link';
+import { useUser } from '@auth0/nextjs-auth0/client';
+
 export default function Home() {
-  return <div />;
+  const { user } = useUser();
+
+  return (
+    <div>
+      <h1>This is the homepage</h1>
+      <div>
+        {!!user && (
+          <div>
+            <Image src={user.picture} alt={user.name} width={50} height={50} />
+            <div>{user.email}</div>
+          </div>
+        )}
+        {!!user ? (
+          <Link href="/api/auth/logout?returnTo=/api/auth/login">
+            Logout
+          </Link>
+        ) : (
+          <Link href="/api/auth/login">Login</Link>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function Image(props) {
+  return <img {...props} />;
 }
