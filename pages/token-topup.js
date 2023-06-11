9c0a86553de6004b56ab82b91dd648e1744e0939
JoakBouy <B.joak@alustudent.com>
2023-06-11 04:34:55 +0300

    Created Token Topup page

--- pages/token-topup.js
+++ pages/token-topup.js
@@ -1,13 +1,19 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { AppLayout } from '../components/AppLayout';
 
-export default function NewPost(props) {
+export default function TokenTopup(props) {
     console.log('NEW POST PROPS:', props);
     return (
         <div>
-            <h1>This is the new post page</h1>
+            <h1>This is the TokenTopup page</h1>
         </div>
     );
 }
+
+TokenTopup.getLayout = function getLayout(page, pageProps) {
+    return <AppLayout {...pageProps}>{page}</AppLayout>;
+  };
+
 
 export const getServerSideProps = withPageAuthRequired (() => {
     return {
